2d53cde0efdfd551e4acc7fb9f727f7f39d86daa
硕鼠宝 <759979885@qq.com>
2019-05-07 21:20:49 +0800

    Create 3.js

---
+++ 3.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+const homedir = os.homedir();
+
+const CodeDir = path.resolve(homedir, 'Library/Application\ Support/Code/User/settings.json');
+
+console.log(fs.readFileSync(CodeDir).toString());
